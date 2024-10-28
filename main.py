bf9491467edf04a49b5b574116b6ca58188d6dd9
Michael Hüppe <michael.hueppe@snap-gmbh.com>
2024-10-28 12:42:30 +0100

    Add icon to application

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # author: Michael Hüppe
 # date: 28.10.2024
 # project: /main_application.py
+from PySide2.QtGui import QIcon
 
 # local
 from gui.interface import Interface
@@ -38,9 +39,36 @@ class MainApplication(QMainWindow):
         self.interface.handleOutput(output)
 
 
+# Define the stylesheet
+stylesheet = """
+    QWidget {
+        background-color: #D3D3D3;  /* Light grey background */
+    }
+
+    QPushButton {
+        background-color: #cd2f2d;  /* Red background for buttons */
+        color: white;               /* White text color */
+        border-radius: 15px;        /* Rounded edges */
+        padding: 10px;              /* Internal padding */
+        font-size: 16px;            /* Font size */
+    }
+
+    QPushButton:hover {
+        background-color: #CC0000; /* Darker red on hover */
+    }
+
+    QPushButton:pressed {
+        background-color: #990000; /* Even darker red on press */
+    }
+"""
+
 def main():
     """Main entry point for the application."""
     app = QApplication(sys.argv)
+    app.setStyleSheet(stylesheet)
+    # Set the application icon (modify the path to your icon file)
+    icon_path = "gui/media/UHH_Universitaet_Hamburg_Logo.png"  # Provide the correct path to your PNG image
+    app.setWindowIcon(QIcon(icon_path))
     main_window = MainApplication()
     main_window.show()
     sys.exit(app.exec_())
