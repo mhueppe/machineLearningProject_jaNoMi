0670d5fc9793fd83f8cd697f6e9be4e309737f18
dasjannishuh <jannis.waller@web.de>
2024-12-08 21:35:32 +0100

    refactor and merge hyperparameter tuning for RNN and transformer, replacing individual tunings

--- training.py
+++ training.py
@@ -3,51 +3,15 @@ import logging
 import os
 
 import tensorflow as tf
-from resources.createModel import createModel, init_tokenizers
-from resources.dataPreprocessing import vectorize_text
-from utils.util_readingData import split_datasets, load_data, calc_params
 
-AUTOTUNE = tf.data.AUTOTUNE
-
-# TODO: load in funcs, pass as param?
-with open(os.path.join("resources","params.json")) as f:
-    params = json.load(f)
-
-def split_data(titles, abstracts):
-    train_abs, train_titles, val_abs, val_titles, test_abs, test_titles = split_datasets(abstracts, titles)
-
-    train_dataset = tf.data.Dataset.from_tensor_slices((train_abs, train_titles))
-    val_dataset = tf.data.Dataset.from_tensor_slices((val_abs, val_titles))
-    test_dataset = tf.data.Dataset.from_tensor_slices((test_abs, test_titles))
-
-    logging.info(f"number of training samples: {len(train_dataset)}")
-    logging.info(f"number of validation samples: {len(val_dataset)}")
-    logging.info(f"number of test samples: {len(test_dataset)}")
-
-    context_tokenizer, target_tokenizer = init_tokenizers(titles, abstracts)
-    def vectorize_text_with_tokenizer(contexts, targets):
-        return vectorize_text(contexts, targets,
-                              context_tokenizer, target_tokenizer)
-
-    train_dataset = (
-        train_dataset.
-        map(vectorize_text_with_tokenizer, num_parallel_calls=AUTOTUNE).
-        shuffle(params["buffer_size"], seed=params["SEED"]).
-        batch(params["batch_size"], drop_remainder=True).
-        prefetch(AUTOTUNE)
-    )
-
-    val_dataset = (
-        val_dataset.
-        map(vectorize_text_with_tokenizer, num_parallel_calls=AUTOTUNE).
-        batch(params["batch_size"], drop_remainder=True).
-        prefetch(AUTOTUNE)
-    )
-    # TODO: return test_dataset? add tokenization
-    return train_dataset, val_dataset, test_dataset
+from hyperparameterTuning import split_data
+from resources.createModel import init_model
+from utils.util_readingData import load_data
 
 
 if __name__ == "__main__":
+    with open(os.path.join("resources", "headliner-params.json")) as f:
+        params = json.load(f)
     logging.basicConfig(level=logging.INFO)
     tf.keras.backend.clear_session() #Clearing Keras memory
     tf.random.set_seed(params["SEED"]) #For reproducibility
@@ -59,10 +23,9 @@ if __name__ == "__main__":
     else:
         logging.info("No GPU found")
 
-    titles, abstracts = load_data()
-    calc_params(abstracts, titles)
-    train_dataset, val_dataset, _ = split_data(titles, abstracts)
-    model = createModel()
+    titles, abstracts = load_data('Arxiv', params)
+    train_dataset, val_dataset, _ = split_data(titles, abstracts, params)
+    model = init_model(params)
 
     # Callback to stop training early if accuracy does not increase for 2 epochs
     callback = tf.keras.callbacks.EarlyStopping(
